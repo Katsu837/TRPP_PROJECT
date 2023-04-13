f6a4fa461a296185957d9bef5d99c9cd413f4cb9
Katsu837 <mp.200402@gmail.com>
2023-04-13 14:36:13 +0300

    add Readme

--- src/App.js
+++ src/App.js
@@ -4,15 +4,20 @@ import RegistrationEntry from "./components/RegistrationEntry";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Entry from "./components/registrationEntryComponents/Entry";
 import Registration from "./components/registrationEntryComponents/Registration";
+import BigCalendar from "./components/BigCalendar";
 
 function App() {
   return (
-    <BrowserRouter className="App">
-      <Routes>        
-        <Route path="/" element={<Entry/>}></Route>
-        <Route path="/reg" element={<Registration/>}></Route>
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter className="App">
+    //   <Routes>
+    //     <Route path="/" element={<Entry/>}></Route>
+    //     <Route path="/reg" element={<Registration/>}></Route>
+    //   </Routes>
+    // </BrowserRouter>
+      <div className="App">
+        <SmallCalendar/>
+        <BigCalendar/>
+      </div>
   );
 }
 
