23b9eaef73f8ca9d953ae2d9ec7e53b7aa0d6c11
Grigor <120247466+LevVasilev@users.noreply.github.com>
2023-04-25 15:27:13 +0300

    Update FriendList.jsx

--- src/components/Friendsaddsearchopen/FriendList.jsx
+++ src/components/Friendsaddsearchopen/FriendList.jsx
@@ -1,55 +1,83 @@
-import React from "react";
-import Friend from "./Friend";
+import React, { useState } from 'react';
+import lionshop from "../image/lionshop.jpg"
+import people from "../image/people.png"
+import garbage from "../image/garbage.png"
 
-function FriendsList({ friends, onRemoveFriend, onOpenCalendar }) {
-  const handleRemoveFriend = (friendId) => {
-    onRemoveFriend(friendId);
-  };
+const friends = [
+  {
+    id: 1,
+    name: 'Иван Иванов',
+    avatar: 'path/to/avatar1.jpg'
+  },
+  {
+    id: 2,
+    name: 'Петр Петров',
+    avatar: 'path/to/avatar2.jpg'
+  },
+  {
+    id: 3,
+    name: 'Анна Сидорова',
+    avatar: 'path/to/avatar3.jpg'
+  },
+  {
+    id: 4,
+    name: 'Мария Кузнецова',
+    avatar: 'path/to/avatar4.jpg'
+  },
+  {
+    id: 3,
+    name: 'Анна Сидорова',
+    avatar: 'path/to/avatar3.jpg'
+  },
+  {
+    id: 3,
+    name: 'Анна Сидорова',
+    avatar: 'path/to/avatar3.jpg'
+  },
+  {
+    id: 3,
+    name: 'Анна Сидорова',
+    avatar: 'path/to/avatar3.jpg'
+  },
+  {
+    id: 3,
+    name: 'Анна Сидорова',
+    avatar: 'path/to/avatar3.jpg'
+  }
+];
 
-  const handleOpenCalendar = (friend) => {
-    onOpenCalendar(friend);
-  };
+const FriendList = () => {
+  const [myFriends, setMyFriends] = useState(friends);
 
-  const handleContextMenu = (event, friend) => {
-    event.preventDefault();
-    // открыть контекстное меню для удаления друга
+  const handleAddFriend = (friendId) => {
+    const friendIndex = myFriends.findIndex((friend) => friend.id === friendId);
+    if (friendIndex !== -1) {
+      const updatedFriends = [...myFriends];
+      updatedFriends[friendIndex] = { ...updatedFriends[friendIndex], added: true };
+      setMyFriends(updatedFriends);
+    }
   };
 
   return (
     <div>
-      <h2 className="Listname">Список друзей:</h2>
-      {friends && friends.length > 0 ? (
-        friends.map((friend) => (
-          <div
-            key={friend.id}
-            className="friend"
-            onContextMenu={(event) => handleContextMenu(event, friend)}
-          >
-            <Friend
-              friend={friend}
-              onRemoveFriend={handleRemoveFriend}
-              onOpenCalendar={handleOpenCalendar}
-            />
+      <div className="Listandbtn"><h2>Список друзей</h2><button className="somebtn1"><img className="peopleimg" src={people}  /></button></div>
+    <div className="friends-list-container">
+      <div className="friends-list">
+        {myFriends.map((friend) => (
+          <div key={friend.id} className="friend">
+            <img className='Avatar' src={friend.avatar} alt="Avatar" />
+            <p className="name">{friend.name}</p>
+            {!friend.added ? (
+              <button className='somebtn' onClick={() => handleAddFriend(friend.id)}><img className="peopleimg" src={garbage}  /></button>
+            ) : (
+              <p>Добавлен</p>
+            )}
           </div>
-        ))
-      ) : (
-        <div className="Friends">
-        <div className="Friendprof">
-          <div className="Circle"></div>
-        <p className="Friendname">Друг для примера</p>
-
-        </div>
-
-        <div className="Friendprof"><div className="Circle"></div>
-        <p className="Friendname">Друг для примера</p></div>
-        <div className="Friendprof"><div className="Circle"></div>
-        <p className="Friendname">Друг для примера</p></div>
-        <div className="Friendprof"><div className="Circle"></div>
-        <p className="Friendname">Друг для примера</p></div>
-        </div>
-      )}
+        ))}
+      </div>
+    </div>
     </div>
   );
-}
+};
 
-export default FriendsList;
+export default FriendList;
